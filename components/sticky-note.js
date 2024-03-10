3c97736f6fc4f56cc505af0a9ac294346e5e3401
sossopyc <cypossosmu51c@gmail.com>
2024-03-10 11:41:50 +0100

    Made sticky note with close button

--- components/sticky-note.js
+++ components/sticky-note.js
@@ -3,30 +3,62 @@ class StickyNote extends HTMLElement {
         super();
 
         this.shadow = this.attachShadow({ mode: "open" });
-
+        this.width = 200;
         this.shadow.innerHTML =  `
             <style>
+                ${styles}
+
                 #sticky-note {
-                    width: 150px;
+                    width: ${this.width}px;
                     aspect-ratio: 1;
+
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
 
                     background-color: yellow;
                     color: black;
 
-                    transform: rotateZ(30deg);
+                    transform: rotateZ(20deg);
+                }
+
+                button {
+                    width: 20px;
+                    aspect-ratio: 1;
+
+                    border: none;
+
+                    cursor: pointer;
+                    position: absolute;
+                    top: 10px;
+                    right: 10px;
+                }
+
+                #red {
+                    background-color: red;
                 }
             </style>
             <div id="sticky-note">
+                <button id="red"></button>
                 <slot></slot>
             </div>
         `;
     }
 
     connectedCallback() {
-        setTimeout(() => {
+        const redBtn = this.shadow.querySelector("#red");
+        redBtn.addEventListener("click", () => {
+            this.shadow.host.remove();
+        });
+
+        const stickyInterval = setInterval(() => {
+            if (!this.shadow.host) {
+                clearInterval(stickyInterval);
+                return; 
+            }
             const main = document.querySelector("main");
-            this.shadow.host.style.left = main.offsetLeft - 60 + "px";
-            this.shadow.host.style.top = ( main.offsetTop + main.offsetHeight - 75 ) + "px";
+            this.shadow.host.style.left = main.offsetLeft + 20 - this.width / 2 + "px";
+            this.shadow.host.style.top = ( main.offsetTop + main.offsetHeight - this.width / 2 - 20 ) + "px";
             this.shadow.host.style.display = "block";
         }, 0);
     }
