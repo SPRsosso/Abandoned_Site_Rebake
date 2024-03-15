fc616c1f9a8613a2981a08f217efaaa8634ecc0d
sosso <118058499+SPRsosso@users.noreply.github.com>
2024-03-15 16:40:55 +0100

    Did tutorial for commands

--- js/phone/apps/tutorial-components/tutorialcommands.js
+++ js/phone/apps/tutorial-components/tutorialcommands.js
@@ -1,4 +1,26 @@
 const tutorialCommands = `
-    <h3></h3>
-    <p></p>
+    <h2>Ethical commands</h2>
+
+    <h3>help</h3>
+    <p>Shows help advice for default ethical commands</p>
+
+    <h3>clear</h3>
+    <p>It clears console</p>
+
+    <h3>setuser *name*</h3>
+    <p>It will change current username</p>
+
+    <h3>connectwifi *wifi name* *wifi password*</h3>
+    <p>Connect to the given Wifi if Wifi exists and password is true</p>
+
+    <h3>ccodetab</h3>
+    <p>Outputs character code table</p>
+
+    <h3>downapp *app name*</h3>
+    <p>Downloads you given app if app exists</p>
+
+    <h2>Unethical commands</h2>
+
+    <h3>breakwifi *wifi name*</h3>
+    <p>Gives you sequence of numbers which you add after you did this twice you get Wifi password key. With password key you need to translate it with ccodetab</p>
 `;
