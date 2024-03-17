93e49c5022a2acb484b225b02a2ec6363ac21aba
sossopyc <cypossosmu51c@gmail.com>
2024-03-17 17:58:36 +0100

    Click sounds

--- js/sound.js
+++ js/sound.js
@@ -1,5 +1,11 @@
-addEventListener("click", () => {
-    const click = new Audio("./sounds/Click.mp3");
+addEventListener("mousedown", () => {
+    const click = new Audio("./sounds/ClickDown.mp3");
+    click.volume = 0.5;
+    click.play();
+});
+
+addEventListener("mouseup", () => {
+    const click = new Audio("./sounds/ClickUp.mp3");
     click.volume = 0.2;
     click.play();
 });
