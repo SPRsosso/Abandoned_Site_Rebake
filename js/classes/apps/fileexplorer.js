7dcb4af81b513b1c70c2ff18ac3ba191716ebd25
sosso <118058499+SPRsosso@users.noreply.github.com>
2024-03-17 13:21:52 +0100

    Update fileexplorer.js

--- js/classes/apps/fileexplorer.js
+++ js/classes/apps/fileexplorer.js
@@ -108,11 +108,11 @@ class FileExplorer extends App {
                 fileexplorer.currentFolderIndex--;
                 fileexplorer.getInsideFolder(appComponent, Apartment.activeApartment.pc);
 
-                if (fileexplorer.currentFolderIndex == 0) {
+                if (fileexplorer.currentFolderIndex == 0)
                     backward.classList.add("disabled");
-                } else {
-                    backward.classList.add("disabled");
-                }
+                
+            } else {
+                backward.classList.add("disabled");
             }
         });
 
