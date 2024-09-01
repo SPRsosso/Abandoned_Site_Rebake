d8063db6e905abf3c5dd0aad03149af2c8248872
sossopyc <cypossosmu51c@gmail.com>
2024-09-01 14:42:04 +0200

    Reset game to start

--- js/setup.js
+++ js/setup.js
@@ -1,29 +1,28 @@
 // Apartment
 Apartment.activeApartment = apartments[0];
-Apartment.activeApartment.pc.on = true;
-Apartment.activeApartment.pc.loggedIn = true;
+// Apartment.activeApartment.pc.on = true;
+// Apartment.activeApartment.pc.loggedIn = true;
 
 // Downloaded apps
-Apartment.activeApartment.pc.downloadedApps = {
-    CMD,
-    FileExplorer,
-    Calculator,
-    MessX,
-    Browser,
-    Notepad,
-    Canvas,
-    WiTracker,
-    HomeDefence,
-    Personide,
-}
+// Apartment.activeApartment.pc.downloadedApps = {
+//     CMD,
+//     FileExplorer,
+//     Calculator,
+//     MessX,
+//     Browser,
+//     Notepad,
+//     Canvas,
+//     WiTracker,
+//     HomeDefence,
+//     Personide,
+// }
 
 // True setup
-// Apartment.activeApartment.router.connectedWifi = null;
-// Apartment.activeApartment.pc.os.system = null;
-// Apartment.activeApartment.pc.os.version = null;
+Apartment.activeApartment.router.connectedWifi = null;
+Apartment.activeApartment.pc.os.system = null;
+Apartment.activeApartment.pc.os.version = null;
 
 const routers = Apartment.activeApartment.routers;
-
 
 let wifiIndex = 0;
 Apartment.activeApartment.wifis.forEach(( wifi, index ) => {
